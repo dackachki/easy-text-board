acd80bf21f749b5855f71c72ae8de48a4dc8e81f
dackachki <skatmddnjs77@gmail.com>
2020-10-12 12:52:47 +0900

    while&if

--- src/com/sbs/example/eastextboard/Main.java
+++ src/com/sbs/example/eastextboard/Main.java
@@ -1,8 +1,29 @@
 package com.sbs.example.eastextboard;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello");
-	}
+
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.printf("명령어 입력:");
+			String command = scanner.nextLine();
+
+			if (command.equals("article list")) {
+				System.out.println("== 게시물 리스트 ==");
+			} else if (command.equals("article add")) {
+				System.out.println("== 게시물 작성 ==");
+			} else if (command.equals("break")) {
+				break;
+			} else {
+				System.out.println("잘못된 입력");
+			}
+		}
+			scanner.close();
+
+		}
+
 	
+
 }
