de53039c6100507fb95809384386c98448b9c1dc
dackachki <skatmddnjs77@gmail.com>
2020-10-13 12:42:18 +0900

    리스트 두개 내용 저장

--- src/com/sbs/example/eastextboard/Main.java
+++ src/com/sbs/example/eastextboard/Main.java
@@ -1,63 +1,72 @@
 package com.sbs.example.eastextboard;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		int id = 1;
-	
-		while(true){
-			
-			
-				
+		int pre_id = 0;
+
+		int article1_id = 0;
+		String article1_title = "";
+		String article1_body = "";
+
+		int article2_id = 0;
+		String article2_title = "";
+		String article2_body = "";
+
+		while (true) {
+
 			System.out.printf("명령어 :");
-		
-		String command = scanner.nextLine();
-		
-		
-		
-		if (command.equals("article list")) {
-			System.out.println("==게시판 리스트==");
 
-		}
-		else if (command.equals("aaa")) {
-			System.out.println("==게시판 업로드==");
-			
+			String command = scanner.nextLine();
 
-			String title;
-			String body;
-			
-			
-			System.out.printf("제목 : ");
-			title = scanner.nextLine();
-			System.out.printf("내용 : ");
-			body = scanner.nextLine();
-			
+			if (command.equals("article upload")) {
+				System.out.printf("타이틀 : ");
+				String title = scanner.nextLine();
 
+				System.out.printf("내용 : ");
+				String body = scanner.nextLine();
 
-		
-			System.out.printf("게시물 번호 :%d\n",id);
-			System.out.println("게시물 제목 : "+title);
-			System.out.println("게시물 내용 : "+body);
-			id ++;
-			
-			
-		}
-		else if (command.equals("exit")){
-			
-			break;
-		}
-		else {
-			System.out.println("wrong");
+				System.out.println("== 게시물 정보 == ");
+				System.out.println("번호 : " + id);
+				System.out.println("제목 : " + title);
+				System.out.println("내용 : " + body);
 
-		}
+				if (id == 1) {
+					 article1_id = id;
+					 article1_title = title;
+					 article1_body = body;
+				}
+
+				else if (id == 2) {
+					 article2_id = id;
+					 article2_title = title;
+					 article2_body = body;
+				}
+				id++;
+				pre_id++;
+
+			} else if (command.equals("article list")) {
+				if (pre_id == 0) {
+					System.out.println("게시물이 없습니다");
+				} else {
+					if( pre_id >= 1) {
+						System.out.printf("%d / %s\n / %s",article1_id,article1_title,article1_body);
+					}
+					if( pre_id >= 2) {
+						System.out.printf("%d / %s\n / %s ",article2_id,article2_title,article2_body);
+					}
+				}
+			} else if (command.equals("exit")) {
+				System.out.println("== 프로그램 종료 ==");
+				break;
+			}
 		}
 		scanner.close();
-	
-		
-}
+
+	}
 }
