fbb54965c2d100d78eac0adbc940496d7cd95970
skatmddnjs77 <skatmddnjs77@gmail.com>
2020-10-13 09:57:50 +0900

    게시물 업로드

--- src/com/sbs/example/eastextboard/Main.java
+++ src/com/sbs/example/eastextboard/Main.java
@@ -4,26 +4,60 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-
+		
+		
 		Scanner scanner = new Scanner(System.in);
-		while (true) {
-			System.out.printf("명령어 입력:");
-			String command = scanner.nextLine();
-
-			if (command.equals("article list")) {
-				System.out.println("== 게시물 리스트 ==");
-			} else if (command.equals("article add")) {
-				System.out.println("== 게시물 작성 ==");
-			} else if (command.equals("break")) {
-				break;
-			} else {
-				System.out.println("잘못된 입력");
-			}
-		}
-			scanner.close();
-
-		}
-
+		
+		int id = 1;
 	
+		while(true){
+			
+			
+				
+			System.out.printf("명령어 :");
+		
+		String command = scanner.nextLine();
+		
+		
+		
+		if (command.equals("article list")) {
+			System.out.println("==게시판 리스트==");
 
+		}
+		else if (command.equals("aaa")) {
+			System.out.println("==게시판 업로드==");
+			
+
+			String title;
+			String body;
+			
+			
+			System.out.printf("제목 : ");
+			title = scanner.nextLine();
+			System.out.printf("내용 : ");
+			body = scanner.nextLine();
+			
+
+
+		
+			System.out.printf("게시물 번호 :%d\n",id);
+			System.out.println("게시물 제목 : "+title);
+			System.out.println("게시물 내용 : "+body);
+			id ++;
+			
+			
+		}
+		else if (command.equals("exit")){
+			
+			break;
+		}
+		else {
+			System.out.println("wrong");
+
+		}
+		}
+		scanner.close();
+	
+		
+}
 }
