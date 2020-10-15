a57e02e619683e4b3a08b3002a0ee3460e5b4138
dackachki <skatmddnjs77@gmail.com>
2020-10-15 13:48:24 +0900

    App class 추가

---
+++ src/com/sbs/example/eastextboard/App.java
@@ -0,0 +1,81 @@
+package com.sbs.example.eastextboard;
+
+import java.util.Scanner;
+
+public class App {
+
+	public void run() {
+		Scanner scanner = new Scanner(System.in);
+
+		Article[] art = new Article[10];
+		art[0] = new Article();
+		art[1] = new Article();
+		art[2] = new Article();
+		art[3] = new Article();
+		art[4] = new Article();
+		art[5] = new Article();
+		art[6] = new Article();
+		art[7] = new Article();
+		art[8] = new Article();
+		art[9] = new Article();
+
+		int i = 0;
+		int id = 1;
+		while (true) {
+			art[i] = new Article();
+
+			System.out.printf("명령어 :");
+
+			String command = scanner.nextLine();
+
+			if (command.equals("article add")) {
+
+				System.out.printf("타이틀 : ");
+				String title = scanner.nextLine();
+				art[i].title = title;
+				System.out.printf("내용 : ");
+				String body = scanner.nextLine();
+				art[i].body = body;
+				art[i].id = id;
+				System.out.println("== 게시물이 생성됨 ==");
+				System.out.printf("%d번", art[i].id);
+				System.out.printf("제목 : %s\n", art[i].title);
+				System.out.printf("내용 : %s\n", art[i].body);
+				i++;
+				id++;
+			} else if (command.equals("article list")) {
+				for (int j = 0; j < art.length; j++) {
+					System.out.println("Index / Title / Body");
+					System.out.printf("%d번", art[j].id);
+					System.out.printf("     %s", art[j].title);
+					System.out.printf("      %s\n", art[j].body);
+				}
+			}  
+			
+			else if (command.startsWith("article detail")) {
+				
+				String[] commandbits =command.split(" ");
+				int iddef = Integer.parseInt(commandbits[2]);
+				
+				System.out.println("== 게시물 상세보기 ==");
+				System.out.printf("%d번\n", art[id].id);
+				System.out.printf("%s\n", art[id].title);
+				System.out.printf("%s\n", art[id].body);
+				
+			
+			}
+			else if (command.equals("exit")) {
+				break;
+			}
+			else  {
+				System.out.println("잘못된 명령어 입니다.");
+			}
+			}
+			
+		
+		
+		scanner.close();
+		
+	}
+
+}
--- src/com/sbs/example/eastextboard/Main.java
+++ src/com/sbs/example/eastextboard/Main.java
@@ -4,60 +4,9 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
+		new App().run();
 
-		Scanner scanner = new Scanner(System.in);
-
-		Article[] art = new Article[10];
-		art[0] = new Article();
-		art[1] = new Article();
-		art[2] = new Article();
-		art[3] = new Article();
-		art[4] = new Article();
-		art[5] = new Article();
-		art[6] = new Article();
-		art[7] = new Article();
-		art[8] = new Article();
-		art[9] = new Article();
-
-		int i = 0;
-		int id = 1;
-		while (true) {
-			art[i] = new Article();
-
-			System.out.printf("명령어 :");
-
-			String command = scanner.nextLine();
-
-			if (command.equals("article add")) {
-
-				System.out.printf("타이틀 : ");
-				String title = scanner.nextLine();
-				art[i].title = title;
-				System.out.printf("내용 : ");
-				String body = scanner.nextLine();
-				art[i].body = body;
-				art[i].id = id;
-				System.out.println("== 게시물이 생성됨 ==");
-				System.out.printf("%d번", art[i].id);
-				System.out.printf("제목 : %s\n", art[i].title);
-				System.out.printf("내용 : %s\n", art[i].body);
-				i++;
-				id++;
-			} else if (command.equals("article list")) {
-				for (int j = 0; j < art.length; j++) {
-					System.out.println("Index / Title / Body");
-					System.out.printf("%d번", art[j].id);
-					System.out.printf("     %s", art[j].title);
-					System.out.printf("       %s\n", art[j].body);
-				}
-			} else if (command.equals("exit")) {
-				break;
-			} else {
-				System.out.println("잘못된 명령어 입니다.");
-			}
-
-		}
-		scanner.close();
+		
 
 	}
 
