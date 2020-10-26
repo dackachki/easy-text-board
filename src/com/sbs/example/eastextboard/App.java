a80f616490354061fa4333d2735516c9223c7d5c
dackachki <skatmddnjs77@gmail.com>
2020-10-26 13:42:28 +0900

    검색하기

--- src/com/sbs/example/eastextboard/App.java
+++ src/com/sbs/example/eastextboard/App.java
@@ -28,7 +28,7 @@ public class App {
 	}
 
 	private int add(String title, String body) {
-		
+
 		if (isArticlesFull()) {
 
 			Article[] newArticles = new Article[articles.length * 2];
@@ -87,14 +87,16 @@ public class App {
 	// 가장 상위층 시작
 	public void run() {
 		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 1; i <= articles.length; i++) {
+			add("title" + i, "body" + i);
+		}
 
 		while (true) {
 
-			System.out.printf("명령어) ");
+			System.out.printf("명령어 : ");
 			String command = sc.nextLine();
-			for (int i = 1; i <= articles.length; i++) {
-				add("title" + i, "body" + i);
-			}
+			
 			if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
 				break;
@@ -108,30 +110,30 @@ public class App {
 					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
-				if(pagenum <= 1) {
+				if (pagenum <= 1) {
 					pagenum = 1;
 				}
-				
-				int itemsInPage =10;
-				int StartNum = articlesSize()-1;
-				StartNum -= (pagenum-1) *itemsInPage;
-				int EndNum =StartNum - itemsInPage -1;
-				
+
+				int itemsInPage = 10;
+				int StartNum = articlesSize() - 1;
+				StartNum -= (pagenum - 1) * itemsInPage;
+				int EndNum = StartNum - itemsInPage - 1;
+
 				if (EndNum < 0) {
 					EndNum = 0;
 				}
-				if (StartNum < 0) {
-					System.out.printf("%d번 페이지는 존재하지 않습니다.",pagenum);
-				}
-				for(int i = StartNum; i >=EndNum; i--) {
-					Article article = articles[i];
-					System.out.printf("%d번 / %s\n",article.id,article.title);
-					
-				}
-				continue;
 				
-			} 
-			else if (command.startsWith("article detail ")) {
+				if (StartNum < 0) {
+					System.out.printf("%d번 페이지는 존재하지 않습니다.", pagenum);
+					continue;
+				}
+				
+				for (int i = StartNum; i >= EndNum; i--) {
+					Article article = articles[i];
+					System.out.printf("%d번 / %s  %s\n", article.id, article.title, article.body);
+				}
+
+			} else if (command.startsWith("article detail ")) {
 				int inputedId = Integer.parseInt(command.split(" ")[2]);
 				System.out.println("== 게시물 상세 ==");
 
@@ -180,7 +182,64 @@ public class App {
 				remove(inputedId);
 
 				System.out.printf("%d번 게시물이 삭제되었습니다.\n", inputedId);
+			} else if (command.startsWith("article search")) {
+				String[] commandbits = command.split(" ");
+				String SearchKeyword = commandbits[2];
+				int page = 1;
+				if (commandbits.length >= 4) {
+					page = Integer.parseInt(commandbits[3]);
+				}
+				if (page <= 1) {
+					page = 1;
+				}
+				System.out.println("== 게시물 검색 결과 ==");
+				int count = 0;
+
+				for (int i = 0; i < articlesSize; i++)
+					if (articles[i].body.contains(SearchKeyword)) {
+						count++;
+
+					}
+				Article[] ResultArticles = new Article[count];
+				int ResultArticle_Index = 0;
+				for (int i = 0; i < articles.length; i++) {
+					Article article = articles[i];
+					if (article.body.contains(SearchKeyword)) {
+						ResultArticles[ResultArticle_Index] = article;
+						ResultArticle_Index++;
+					}
+				}
+				if (ResultArticles.length == 0) {
+					System.out.println("검색결과가 존재하지 않습니다.");
+					continue;
+				}
+				System.out.printf("== 검색어 %s , 검색결과(%d) == \n", SearchKeyword, ResultArticle_Index);
+
+				int itemsInPage = 10;
+				int StartNum = ResultArticles.length - 1;
+				StartNum -= (page - 1) * itemsInPage;
+				int EndNum = StartNum - itemsInPage - 1;
+
+				if (EndNum < 0) {
+					EndNum = 0;
+				}
+				if (StartNum < 0) {
+					System.out.printf("%d번 페이지는 존재하지 않습니다.", page);
+					continue;
+				}
+				System.out.println("Id / 제목 / 내용");
+				for (int i = StartNum; i >= EndNum; i--) {
+					Article article = articles[i];
+					System.out.printf("%d번 / %s  %s\n", article.id, article.title, article.body);
+
+				}
+
 			}
+
+			else {
+				System.out.println("명령어가 틀립니다.");
+			}
+
 		}
 
 		sc.close();
