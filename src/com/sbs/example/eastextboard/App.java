210f04349ed017dee10ed394397610232efbb6cf
dackachki <skatmddnjs77@gmail.com>
2020-10-26 21:49:06 +0900

    맴버 리스트 및 로그인

--- src/com/sbs/example/eastextboard/App.java
+++ src/com/sbs/example/eastextboard/App.java
@@ -9,7 +9,9 @@ public class App {
 	List<Article> list = new ArrayList<Article>();
 	int article_index = 0;
 	Scanner sc = new Scanner(System.in);
-
+	List<Member> memberlist = new ArrayList<Member>();
+	int member_index = 0;
+	String input_name ="";
 	public void run() {
 
 		for (int i = 0; i < articleL; i++) {
@@ -24,7 +26,7 @@ public class App {
 				int pagenum = Integer.parseInt(command.split(" ")[2]);
 
 				int itemsInPage = 10;
-				int StartNum = articleL - 1;
+				int StartNum = list.size() - 1;
 				StartNum -= (pagenum - 1) * itemsInPage;
 				int EndNum = StartNum - itemsInPage - 1;
 
@@ -55,7 +57,7 @@ public class App {
 					System.out.printf("%d /  %s  / %s\n", list.get(number).id, list.get(number).title,
 							list.get(number).body);
 				} catch (IndexOutOfBoundsException e) {
-					System.out.printf("%d번 게시물이 존재하지 않습니다.0부터 %d번 까지 입니다\n.", number, articleL);
+					System.out.printf("%d번 게시물이 존재하지 않습니다.0부터 %d번 까지 입니다\n.", number, list.size());
 				} catch (NumberFormatException e) {
 					System.out.println("양의 정수를 입력하세요.");
 
@@ -68,38 +70,36 @@ public class App {
 					input_index = Integer.parseInt(command.split(" ")[2]);
 
 					list.remove(input_index);
-					articleL--;
+
 					System.out.printf("%d번이 삭제 되었습니다.\n", input_index);
 				} catch (IndexOutOfBoundsException e) {
 					System.out.printf("%d번 게시물이 존재하지 않습니다.0부터 %d번 까지 입니다\n.", input_index, articleL);
 				}
 
 			} else if (command.startsWith("article modify")) {
-					int input_index  =0;
+				int input_index = 0;
 				try {
 					input_index = Integer.parseInt(command.split(" ")[2]);
-				
-				System.out.printf("== %d게시물 수정 ==\n", input_index);
-				
-				
-				System.out.println("변경할 제목 입력 : ");
-				String title = sc.nextLine();
-				list.get(input_index).title = title;
-				System.out.println("변경할 내용 입력 : ");
-				String body = sc.nextLine();
-				
-				list.get(input_index).body = body;
-				}
-				 catch (IndexOutOfBoundsException e) {
-						System.out.printf("%d번 게시물이 존재하지 않습니다.0부터 %d번 까지 입니다\n.", input_index, articleL);
-					} catch (NumberFormatException e) {
-						System.out.println("양의 정수를 입력하세요.");
 
-					}
+					System.out.printf("== %d게시물 수정 ==\n", input_index);
+
+					System.out.println("변경할 제목 입력 : ");
+					String title = sc.nextLine();
+					list.get(input_index).title = title;
+					System.out.println("변경할 내용 입력 : ");
+					String body = sc.nextLine();
+
+					list.get(input_index).body = body;
+				} catch (IndexOutOfBoundsException e) {
+					System.out.printf("%d번 게시물이 존재하지 않습니다.0부터 %d번 까지 입니다\n.", input_index, list.size());
+				} catch (NumberFormatException e) {
+					System.out.println("양의 정수를 입력하세요.");
+
+				}
 			} else if (command.startsWith("article search")) {
 				String keywords = command.split(" ")[2];
 
-				for (int i = 0; i < articleL; i++) {
+				for (int i = 0; i < list.size(); i++) {
 					String getbody = list.get(i).body;
 					if (getbody.contains(keywords)) {
 						System.out.printf("%d  / %s   %s\n", list.get(i).id, list.get(i).title, list.get(i).body);
@@ -107,6 +107,57 @@ public class App {
 					}
 				}
 
+			} else if (command.equals("member reg")) {
+
+				System.out.printf("성함을 입력하세요 : ");
+				String name = sc.nextLine();
+				System.out.printf("비밀 번호를 입력하세요 :");
+				String password = sc.nextLine();
+				memberlist.add(new Member(member_index, name, password));
+
+			} else if (command.equals("member list")) {
+				System.out.println("아이디 / 비밀번호");
+				for (int i = 0; i < memberlist.size(); i++) {
+
+					System.out.printf("%s   / %s\n", memberlist.get(i).name, memberlist.get(i).password);
+
+				}
+			} else if (command.equals("member loggin")) {
+				System.out.printf("아이디를 입력하세요 : ");
+				String name = sc.nextLine();
+			
+				for (int i = 0; i < memberlist.size(); i++) {
+					String getname = memberlist.get(i).name;
+					if (getname.contains(name)) {
+			         input_name = memberlist.get(i).name;
+					
+						
+					} else {
+						System.out.println("해당하는 이름이 존재하지 않습니다.");
+						break;
+					}
+
+				}
+				
+				System.out.printf("비밀번호를 입력하세요 : ");
+				
+				String passwd = sc.nextLine();
+				
+				for (int i = 0; i < memberlist.size(); i++) {
+					String lookingf = memberlist.get(i).name; //맴버리스트 이름 넣기
+					
+					if (lookingf.equals(input_name)) {
+						String checkpw = memberlist.get(i).password;
+						
+						if (passwd.equals(checkpw)) {
+							System.out.printf("%s님 환영합니다.\n", lookingf);
+						} else {
+							System.out.println("비밀번호가 맞지 않습니다.");
+							
+							break;
+						}
+					}
+				}
 			}
 
 			else {
---
+++ src/com/sbs/example/eastextboard/Member.java
@@ -0,0 +1,14 @@
+package com.sbs.example.eastextboard;
+
+public class Member {
+	public int index;
+	public String name;
+	public String password;
+	
+	Member(int index,String name,String password){
+		this.index = index;
+		this.name = name;
+		this.password = password;
+	}
+	
+}
